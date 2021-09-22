8a21451b56321a74234211b01612e8fb7bf59829
Ciucur Andrei <ciucur.andrei@gmail.com>
2021-09-22 19:15:51 +0300

    Show joined boards on first page

--- resources/views/workboard/index.blade.php
+++ resources/views/workboard/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+    <h4>Owned Boards</h4>
     <div class="row">
         <div class="col-md-3 mt-2">
             <div data-toggle="modal" data-target="#new-workboard-modal" style="cursor: pointer">
@@ -10,6 +11,7 @@
                 </div>
             </div>
         </div>
+
         @foreach(Auth::user()->workboards as $workboard)
             <div class="col-md-3 mt-2">
                 <a href="{{ route("workboard.show", ["board" => $workboard->id]) }}" class="text-decoration-none">
@@ -20,6 +22,21 @@
             </div>
         @endforeach
     </div>
+
+    @if(Auth::user()->joinedWorkboards->count() > 0)
+    <h4 class="mt-3">Joined Boards</h4>
+    <div class="row">
+        @foreach(Auth::user()->joinedWorkboards as $workboard)
+            <div class="col-md-3 mt-2">
+                <a href="{{ route("workboard.show", ["board" => $workboard->id]) }}" class="text-decoration-none">
+                    <div class="card justify-content-center align-items-center p-2" style="height: 125px; background: #f7f7f7;">
+                        <span class="text-dark" style="font-size: large">{{ $workboard->name }}</span>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+    </div>
+    @endif
 </div>
 
 <div class="modal fade" id="new-workboard-modal" tabindex="-1" role="dialog" aria-labelledby="newWorkboardModalLabel" aria-hidden="true">
